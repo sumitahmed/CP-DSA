25f07fdc8fcae74efd6f4519d10e5d35f81e6c9c
Sk Sumit Ahmed <67518204+sumitahmed@users.noreply.github.com>
2025-12-27 19:40:23 +0530

    update

---
+++ dsa/searching_sorting/bubbl_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int *arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    bubble_sort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
